097e9bd32e9c116ac5ab28ec187fba6fa20f7ceb
grahamssendagire <121952569+grahamssendagire@users.noreply.github.com>
2023-10-10 11:13:08 -0700

    good1

--- week01/scripts/getDates.js
+++
@@ -1,2 +0,0 @@
-document.querySelector('#lastmodified').textContent="Last Modification:"+document.lastModified;
-document.querySelector('#currentyear').textContent="Current Year:"+document.currentyear;
