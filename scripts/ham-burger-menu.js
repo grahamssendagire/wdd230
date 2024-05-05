eb28d411f06206f3db9e5650ceeeb68c58874214
graham ssendagire <ssendagiregraham745@gmail.com>
2024-05-05 10:26:59 +0300

    Activated harnberg button

--- scripts/ham-burger-menu.js
+++ scripts/ham-burger-menu.js
@@ -4,12 +4,3 @@ hamburgerElement.addEventListener('click',()=>{
     navElement.classList.toggle('open');
 });
 
-//function toggleMenu() {
-    //var menu = document.querySelector('.menu');
-    //menu.classList.toggle('show-menu');
-//}
-
-//function closeMenu() {
-    //var menu = document.querySelector('.menu');
-    //menu.classList.remove('show-menu');
-//}
