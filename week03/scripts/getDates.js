e834d6103d9b1f53ef8f88ec58151126ab1d4e0d
graham ssendagire <ssendagiregraham745@gmail.com>
2024-05-11 06:28:41 +0300

    pagevisits added

---
+++ week03/scripts/getDates.js
@@ -0,0 +1,8 @@
+
+function getLastModifiedDate() {
+  var lastModified = new Date(document.lastModified);
+  return lastModified.toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' });
+}
+
+// Insert the last modification date into the HTML element with id 'lastModified'
+document.getElementById('lastModified').textContent = getLastModifiedDate();
