4d13912ce2b17a1bfc0c3a01023c82318f23981d
graham ssendagire <ssendagiregraham745@gmail.com>
2024-05-20 06:54:48 +0300

    GREAT

--- week04/scripts/formValidation.js
+++ week04/scripts/formValidation.js
@@ -1,16 +1,18 @@
 // declaring or instatiating a variable from an id of the html element.
-const pwd1 = document.querySelector("#pwd1");
-const pwd2 = document.querySelector("#pwd2");
+const pwd1 = document.querySelector("#password1");
+const pwd2 = document.querySelector("#password2");
 const fb = document.querySelector("#feedback");
 //creating an event called focusout
-pwd2.addEventListener('focusout',controlar)
+pwd2.addEventListener('focusout', controlar)
 function controlar(){
     console.log('inside the function')
     if(pwd1.value !== pwd2.vale){
-       console.log('values donot match')
-        fb.textContent ="values donot match.Try again"
-
+      console.log('values donot match')
+        fb.textContent ="❗Key Values DO NOT MATCH! Try again"
+		
     }else{
         console.log('values match')
     }
 }
+
+
